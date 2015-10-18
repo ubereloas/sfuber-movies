15f4ac2ae18a528a056e3312ff2478079d37c573
ubereloas <ubereloas@gmail.com>
2015-10-18 11:47:52 +0200

    [WebClient] Use browserify for dependencies

--- web-client-acceptance-test/src/main/groovy/com/sfubermovies/webclient/test/acceptance/env/WebClientProcess.groovy
+++ web-client-acceptance-test/src/main/groovy/com/sfubermovies/webclient/test/acceptance/env/WebClientProcess.groovy
@@ -1,14 +1,28 @@
 package com.sfubermovies.webclient.test.acceptance.env
 
 class WebClientProcess {
-    private Process process
+    private Process mainProcess
 
     void start() {
-        def args = ['C:\\Program Files\\nodejs\\npm.cmd', 'run', 'serve'] as String[]
-        process = Runtime.runtime.exec(args, null, new File('C:\\Users\\Admin\\Desktop\\sfuber-movies\\web-client'))
+        build()
+        mainProcess = serve()
     }
 
     void stop() {
-        ProcessManager.killProcessTree(process)
+        ProcessManager.killProcessTree(mainProcess)
+    }
+
+    private void build() {
+        Process buildProcess = execNpmTask('build')
+        buildProcess.waitFor()
+    }
+
+    private Process serve() {
+        return execNpmTask('serve')
+    }
+
+    private Process execNpmTask(String taskName) {
+        def args = ['C:\\Program Files\\nodejs\\npm.cmd', 'run', taskName] as String[]
+        return Runtime.runtime.exec(args, null, new File('C:\\Users\\Admin\\Desktop\\sfuber-movies\\web-client'))
     }
 }
