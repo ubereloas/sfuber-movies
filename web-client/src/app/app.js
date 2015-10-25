e3a501e3b8a4d8c84f85c0846d439577946ac93e
ubereloas <ubereloas@gmail.com>
2015-10-25 23:30:11 +0100

    [WebClient] Remove debug statement

--- web-client/src/app/app.js
+++ web-client/src/app/app.js
@@ -4,10 +4,4 @@ angular.module('sfuber-movies', ['uiGmapgoogle-maps', 'angucomplete-alt'])
             key: 'AIzaSyBCIgbAqGrq5cq5DTlQFTcJdL9DcKCGpoI',
             v: '3.21'
         });
-    })
-    .run(function (uiGmapIsReady) {
-        // TODO: remove when done debugging
-        uiGmapIsReady.promise(1).then(function (instances) {
-            window.map = instances[0].map;
-        })
     });
